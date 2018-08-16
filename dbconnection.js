2dfe3eef0e3bb7e7c21b9db8435c68a91a171140
yanmife <yanmifeojo@gmail.com>
2018-08-16 19:55:37 +0100

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password : "",
+    password : "dig001flam40.",
     database : "loantrust_db"
 });
 

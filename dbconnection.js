b025b09bfd30a53e5786d718449ab4106244688e
yanmife <yanmifeojo@gmail.com>
2018-08-29 13:54:41 +0100

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,13 +1,14 @@
 const mysql = require("mysql");
 
 // Create Connection
+// Create Connection
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "loantrust",
-    password : "dig001flam40.",
-    database : "loantrust_db"
+    host : "us-cdbr-iron-east-01.cleardb.net",
+    user : "bb5860e1800559",
+    password : "5f1436e6",
+    database : "heroku_2e5ea325b630181"
+    
 });
-
 //Connect
 db.connect((err) => {
     if(err){
